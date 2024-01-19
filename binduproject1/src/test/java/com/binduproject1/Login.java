190bc3a1ccc672e84c857377d9f2fb321e2418eb
Bindu <Bindu@DESKTOP-MPRUJU8>
2024-01-19 13:30:23 +0530

    binduproject1

---
+++ binduproject1/src/test/java/com/binduproject1/Login.java
@@ -0,0 +1,13 @@
+package com.binduproject1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Login {
+	
+	@Test
+	public void username() {
+		Reporter.log("bindu",true);
+	}
+
+}
